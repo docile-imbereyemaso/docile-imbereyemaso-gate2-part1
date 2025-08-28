ced013a91a8a0a3bc9b1a30f5a6602196cba9357
Docile123 <imbereyemasoo@gmail.com>
2025-08-28 23:10:08 +0200

    feat:desing toggle navbar

--- src/main.js
+++ src/main.js
@@ -1,4 +1,20 @@
 import './style.css'
 
+let nav = document.querySelector("#nav");
+document.querySelector("#menu").addEventListener("click",()=>{
+    
+    if(nav.classList.contains("translate-x-160")){
+        nav.classList.remove("translate-x-160");
+    }else{
+        nav.classList.add("translate-x-160")
+    }
+
+
+});
+
+document.querySelector("#close").addEventListener("click",()=>{
+    
+    nav.classList.add("translate-x-160") 
+})
 
 
